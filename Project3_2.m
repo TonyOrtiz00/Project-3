d53bd57d2494a347f0cccb52eec633104e40f2c2
Jonathan <0904544@my.scccd.edu>
2025-12-01 21:35:34 +0000

    First Commit

--- Project3_2.m
+++
@@ -1,4 +0,0 @@
-% Cretae time and velocity array
-t = 0:1:12;
-vel = [0 1 1.8 2.4 2.6 2.7 2.6 2.2 2 1.9 1.8 1.8 1.8];
-% Determine the acceleration function
--- README.md
+++
@@ -1,2 +0,0 @@
-# Project-3
-You want to demonstrate how effectively you can apply the techniques we have learned in this second unit (Weeks 11- 17)
