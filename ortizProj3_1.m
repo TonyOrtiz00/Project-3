7b12b3510b0a3b0202c884c02b9a42a2d9cb6dc7
Tony <tortizrangel1@my.scccd.edu>
2025-12-06 02:00:38 +0000

    fixed user input

--- ortizProj3_1.m
+++ ortizProj3_1.m
@@ -1,5 +1,6 @@
 %System of Equations - Finding the equation of a parabola
 %Ask user for 3 sets of (x,y) coordinates
-points = input("Enter 3 points as [x y;...]");
-x = (:,1);
-y = (:,2);
+points = input("Enter 3 points as [x1 y1; x2 y2; x3 y3]: ");
+x = points(:,1);
+y = points(:,2);
+
